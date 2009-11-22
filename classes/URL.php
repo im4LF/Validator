5c227d6d5367a68d03fbe9f2cfb2624aca34c49e
im4LF <im@4LF.me>
2009-11-22 22:41:48 +0300

    add method URL->build()

--- classes/URL.php
+++ classes/URL.php
@@ -4,26 +4,35 @@ class URL {
 	
 	protected $_raw_url;
 	protected $_path;
-	protected $_params;
 	protected $_action;
 	protected $_state;
+	protected $_params;
 	protected $_view; 
 	
 	function __construct($url) {
-		$this->_raw_url = $url;
-		$this->_params = array();
-		$this->_action = 'default';
-		$this->_state = 'default';
-		$this->_view_type = 'html';
+		if ( is_array($url) ) {
+			$this->_path = $url['path'];
+			$this->_action = $url['action'];
+			$this->_state = $url['state'];
+			$this->_params = $url['params'];
+			$this->_view = $url['view'];
+		}
+		else {
+			$this->_raw_url = $url;			
+			$this->_action = 'default';
+			$this->_state = 'default';
+			$this->_params = array();
+			$this->_view = 'html';
+		}
 	}
 	
 	function parse() {
 		$buf = parse_url($this->_raw_url);
 		$this->_path = $buf['path'];
 		
-		// parse view type
+		// parse view
 		if ( ($start = strrpos($this->_path, '.')) !== false ) {
-			$this->_view_type = substr($this->_path, $start+1);
+			$this->_view = substr($this->_path, $start+1);
 			$this->_path = substr($this->_path, 0, $start);  
 		}
 		
@@ -49,10 +58,36 @@ class URL {
 		$this->_path = preg_replace('/\/+/','/',$this->_path);
 		$this->_path .= $this->_path{strlen($this->_path)-1} == '/'? '' : '/';
 	}
+	
+	function build() {
+		$action_state = !empty($this->_action) ? '.'.$this->_action : '';
+		$action_state .= !empty($this->_state) ? '.'.$this->_state : '';
+		$this->_raw_url = $this->_path.'/'.$action_state;
+		
+		$params = array();
+		foreach ( $this->_params as $key => $value ) {
+			$params[] = '-'.$key.'/'.$value;
+		}
+		$this->_raw_url .= '/'.implode('/', $params);
+		
+		$this->_raw_url = preg_replace('/\/+/','/',$this->_raw_url);
+		if ( $this->_raw_url{strlen($this->_raw_url)-1} == '/' )
+			$this->_raw_url = substr($this->_raw_url, 0, strlen($this->_raw_url)-1);
+			
+		$this->_raw_url .= '.'.$this->_view;
+	}
 
 }
 
-$url = new URL('http://domain.tld/path/to/some-object/.action.some-state/-qwe/value/-asd/value2/-other-key.json');
-$url->parse();
+//$url = new URL('http://domain.tld/path/to/some-object/.action.some-state/-qwe/value/-asd/value2/-other-key.json');
+//$url->parse();
+$url = new URL(array(
+	'path' => '/this/is/a/path/to/some-object',
+	'action' => 'edit',
+	'state' => 'done',
+	'params' => array('key'=>'value','qwe'=>'qwe'),
+	'view' => 'html'
+));
+$url->build();
 print_r($url);
 ?>
